710d8173dd4311c34944cf495e366f4426927d92
SURYAKANT <suryakant@MacBook-Air.local>
2020-07-15 18:30:59 +0530

    initial commit

---
+++ src/com/suryakant/hackerrank/Day.java
@@ -0,0 +1,9 @@
+package com.suryakant.hackerrank;
+
+public class Day {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+}
---
+++ src/com/suryakant/hackerrank/Day2.java
@@ -0,0 +1,5 @@
+package com.suryakant.hackerrank;
+
+public class Day2 {
+
+}
